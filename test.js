4a1645fd047946d3f7306aafe32d7d82b396abfc
Jean van Kasteel <vankasteelj@gmail.com>
2021-11-02 13:16:29 +0100

    add a small test file

---
+++ test.js
@@ -0,0 +1,21 @@
+const apikey = ''
+const username = ''
+const password = ''
+
+// Load module
+const got = require('got')
+const os = new (require('./opensubtitles.js'))({apikey: apikey})
+
+// Log-in
+os.login({username: username,password: password}).then(response => {
+  // search for 'steal this film 2006'
+  return os.subtitles({query: 'steal this film 2006'})
+}).then(response => {
+  // get the download link for the first result
+  return os.download({file_id: response.data[0].attributes.files[0].file_id})
+}).then(response => {
+  console.log('response.body is', response)
+}).catch(err => {
+  console.log('error message:', err.message)
+  console.log('response is:', err.response)
+})
